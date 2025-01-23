43ce0a1df4c7e5215852cdde287bac85103b50fa
rifatofficial229 <rifatofficial229@gmail.com>
2025-01-23 10:33:08 +0600

    Update 0Support.js

--- scripts/cmds/0Support.js
+++ scripts/cmds/0Support.js
@@ -19,7 +19,7 @@ module.exports = {
 
   // onStart is a function that will be executed when the command is executed
   onStart: async function ({ api, args, message, event }) {
-    const supportGroupId = "8092523114134205"; // ID of the support group
+    const supportGroupId = "6679378595448995"; // ID of the support group
 
     const threadID = event.threadID;
     const userID = event.senderID;
